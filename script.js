042257272d17d943995b4f4d04ab365e4797249b
vaszii <vaszilko.marton07@gmail.com>
2023-10-24 11:22:46 +0200

    Készen van

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("button");
 button.addEventListener("click", createResult);
 
+const type = {
+    "adult" : 350,
+    "student" : 175,
+    "retired" : 100,
+    "animal" : 50
+}
+
 function createResult(){
     const quantity = document.querySelector(".myContainer div input");
     const ticketType = document.querySelector(".myContainer div:nth-child(2) select");
@@ -15,8 +22,21 @@ function createResult(){
     }
     */
     /*Ternary operator: kifejezés ? Ha igaz : Ha hamis */
-    discount = quantity.value >= 10 ? 0.9 : 1;
-    result.value = ticketType.value === "adult" ? `${quantity.value*350 * discount} Ft` : `${quantity.value * 175 * discount} Ft`;
     
+    discount = quantity.value >= 10 ? 0.9 : 1;
+    
+    /*
+    result.value = ticketType.value === "adult" ? `${quantity.value*350 * discount} Ft` : `${quantity.value * 175 * discount} Ft`; 
+    */
+
+    if(ticketType.value === "animal" && quantity.value > 1){
+        quantity.value = "";
+        quantity.focus();
+        discount = 1;
+        alert("Jó, hogy nem akarsz egy egész állatkertet!")
+    }
+    //Amikor több állatot írnak be, akkor a quantity.value értéke üres string lesz.
+    //Mégis működik a szorzás itt lent! Mert "" --> 0 lesz! Inplicit konverzió!
+    result.value = quantity.value * discount * type[ticketType.value];
 
 }
